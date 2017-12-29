b65dd62289b5682b9a44c38d87cab259bf5a87ff
Tawsif Ahmed <ahmedt93@gmail.com>
2017-12-28 23:36:01 -0500

    split test in to 2. second test tests the slots list in the instance

--- tests/test_parking_lot.py
+++ tests/test_parking_lot.py
@@ -4,6 +4,19 @@ sys.path.insert(0, myPath + '/../')
 from parking_lot import create_parking_lot
 
 def test_parking_lot_instantiation():
-    lot1 = create_parking_lot(5)
+    lot1 = create_parking_lot(0)
     assert isinstance(lot1, create_parking_lot)
+
+def test_parking_lot_slots():
+    lot1 = create_parking_lot(5) 
+    lot2 = create_parking_lot(100)   
+    assert isinstance(lot1.slots, list)    
     assert len(lot1.slots) == 5
+    assert isinstance(lot2.slots, list)
+    assert len(lot2.slots) == 100
+
+
+    
+
+
+
