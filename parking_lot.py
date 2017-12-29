5196dc0ec1e04658e9a30bdf176b021413fc1285
Tawsif Ahmed <ahmedt93@gmail.com>
2017-12-29 15:04:55 -0500

    modified parse_user_input to display_options_and_parse_user_input

--- parking_lot.py
+++ parking_lot.py
@@ -1,6 +1,8 @@
 import os
 from create_parking_lot import create_parking_lot
 
+# This is a console app that allows management of parking
+
 ### FUNCTIONS ###
 
 def display_title_bar():
@@ -10,31 +12,38 @@ def display_title_bar():
     print("\t***  Welcome to Sif's parking lot program. How can we help you?  ***")
     print("\t**********************************************")
     
-def parse_user_input():
+def display_options_and_parse_user_input(isLotCreated):
     # Let users know what they can do.
-    print('[create_parking_lot] <int slots>')
-    print('[park] <string registration> <string color>')
-    print('[q] <string quit>')
+    if isLotCreated is True:
+        print('[park] <string registration> <string color>')
+        print('[q] <string quit>')
+    else:
+        print('[create_parking_lot] <int slots>')
+        print('[q] <string quit>')
     # Split string into a list delimited by white spaces
     return input("What would you like to do? ").split()
 
 def initialize_console_app():
     # Set up a loop where users can choose what they'd like to do.
     choice = ['']
+    isLotCreated = False
     display_title_bar()
     while choice[0] != 'q':    
-        choice = parse_user_input()
+        choice = display_options_and_parse_user_input(isLotCreated)
         # Respond to the user's input.
         display_title_bar()
-        if choice[0] == 'create_parking_lot':
-            print("\nParking lot created!\n")
-        elif choice[0] == 'park':
-            print("\nCar parked!\n")
-        elif choice[0] == 'q':
-            print("\nThanks for using the parking lot program!")
+        if isLotCreated == False:
+            if choice[0] == 'create_parking_lot':
+                isLotCreated = True
+                print("\nParking lot created!\n")
         else:
-            print(choice)
-            print("\nI didn't understand that input.\n")
+            if choice[0] == 'park':
+                print("\nCar parked!\n")
+            elif choice[0] == 'q':
+                print("\nThanks for using the parking lot program!")
+            else:
+                print(choice)
+                print("\nI didn't understand that input.\n")
 
 initialize_console_app()
 
