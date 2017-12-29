0e38afbe78aa65c52fb9b8574e8f6d969c9440c8
Tawsif Ahmed <ahmedt93@gmail.com>
2017-12-29 15:22:10 -0500

    implemented functionality to allow a user to create a parking lot of n number of slots through the console

--- parking_lot.py
+++ parking_lot.py
@@ -1,7 +1,7 @@
 import os
 from create_parking_lot import create_parking_lot
 
-# This is a console app that allows management of parking
+# This is a console app that allows management of parking.
 
 ### FUNCTIONS ###
 
@@ -12,9 +12,9 @@ def display_title_bar():
     print("\t***  Welcome to Sif's parking lot program. How can we help you?  ***")
     print("\t**********************************************")
     
-def display_options_and_parse_user_input(isLotCreated):
-    # Let users know what they can do.
-    if isLotCreated is True:
+def display_options_and_parse_user_input(lot):
+    # Let users know what they can do based on whether they have created a lot or not.
+    if isinstance(lot, create_parking_lot) :
         print('[park] <string registration> <string color>')
         print('[q] <string quit>')
     else:
@@ -24,15 +24,17 @@ def display_options_and_parse_user_input(isLotCreated):
     return input("What would you like to do? ").split()
 
 def initialize_console_app():
-    # Set up a loop where users can choose what they'd like to do.
+    # Initialize choice to be an array with a single empty string for command line arguments parsing.
     choice = ['']
-    isLotCreated = False
+    # Initialize isLotCreated state for better UX and program robustness.
+    lot = None
     display_title_bar()
+    # Set up a loop where users can choose what they'd like to do.    
     while choice[0] != 'q':    
-        choice = display_options_and_parse_user_input(isLotCreated)
-        # Respond to the user's input.
+        choice = display_options_and_parse_user_input(lot)
+        # Respond to the user's input based on whether they created a lot or not.
         display_title_bar()
-        if isLotCreated == True:
+        if isinstance(lot, create_parking_lot):
             if choice[0] == 'park':
                 print("\nCar parked!\n")
             elif choice[0] == 'q':
@@ -42,7 +44,8 @@ def initialize_console_app():
                 print("\nI didn't understand that input.\n")
         else:
             if choice[0] == 'create_parking_lot':
-                isLotCreated = True
+                numOfSlots = int(choice[1])
+                lot = create_parking_lot(numOfSlots)
                 print("\nParking lot created!\n")
             else:
                 print(choice)
